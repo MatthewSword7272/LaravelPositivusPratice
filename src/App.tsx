cf6bb242a1dd9e4185f8f2ba0e0fb5bf77ff5727
Matthew Catalfamo <89117055+MatthewSword7272@users.noreply.github.com>
2025-05-21 15:44:37 +1000

    Added Refercnce

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,16 @@ import Footer from "./components/sections/Footer";
 function App() {
   return (
     <>
+      <div className="w-full fixed top-0 text-center bg-green z-[100] py-1">
+        This is design is from{" "}
+        <a
+          className="underline hover:text-green-500"
+          href="https://www.figma.com/design/ICWBzubYlH2NrX9naog0vf/Positivus-Landing-Page-Design--Community-?node-id=330-762&t=joLE1ekrnpBQfdqN-0"
+          target="_blank"
+        >
+          Figma
+        </a>
+      </div>
       <main className={"container mx-auto lg:px-10 px-5 space-y-16"}>
         <Header></Header>
         <HeroSection />
